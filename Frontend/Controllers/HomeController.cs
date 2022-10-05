0bdeb16366810e5e4e6fa330e0a2cfc2866da70c
redmojo7 <donaldchoi5180@gmail.com>
2022-10-05 17:37:16 +0800

    refresh page for frontend

--- Frontend/Controllers/HomeController.cs
+++ Frontend/Controllers/HomeController.cs
@@ -12,9 +12,17 @@ namespace Frontend.Controllers
         public IActionResult Index()
         {
             List<ClientInfo> clientInfos = new List<ClientInfo>();
+            clientInfos.Add(new ClientInfo(1, "localhost", 8080, 10));
+            clientInfos.Add(new ClientInfo(2, "localhost", 8082, 2));
+            //clientInfos = loadClientInfo();
 
-            //clientInfos.Add(new ClientInfo(1, "localhost", 8080, 10));
-            //clientInfos.Add(new ClientInfo(2, "localhost", 8082, 2));
+            return View(clientInfos);
+        }
+
+
+        private List<ClientInfo> loadClientInfo()
+        {
+            List<ClientInfo> clientInfos = new List<ClientInfo>();
 
             // all clients
             RestClient restClient = new RestClient("http://localhost:60238/");
@@ -24,22 +32,42 @@ namespace Frontend.Controllers
             List<Backend.Models.Client> clients = JsonConvert.DeserializeObject<List<Backend.Models.Client>>(restResponse.Content);
             foreach (Backend.Models.Client client in clients)
             {
-                ChannelFactory<JobServerInterface> foobFactory;
-                NetTcpBinding netTcpBinding = new NetTcpBinding();
                 //Set the URL and create the connection!
                 string URL = "net.tcp://" + client.Host + ":" + client.Port + "/JobServer";
-                Console.WriteLine($"dowmloadJob from {URL}");
-                foobFactory = new ChannelFactory<JobServerInterface>(netTcpBinding, URL);
-                JobServerInterface foob = foobFactory.CreateChannel();
-                int numCompletedJobs = 0;
-                foob.FetchJobInfo(out numCompletedJobs);
-                foobFactory.Close();
+                try
+                {
+                    ChannelFactory<JobServerInterface> foobFactory;
+                    NetTcpBinding netTcpBinding = new NetTcpBinding();
+                    //Set the URL and create the connection!
+                    Console.WriteLine($"dowmloadJob from {URL}");
+                    foobFactory = new ChannelFactory<JobServerInterface>(netTcpBinding, URL);
+                    JobServerInterface foob = foobFactory.CreateChannel();
+                    int numCompletedJobs = 0;
+                    foob.FetchJobInfo(out numCompletedJobs);
+                    foobFactory.Close();
 
-                // add into clientInfos
-                clientInfos.Add(new ClientInfo(client.Id, client.Host, client.Port, numCompletedJobs));
+                    // add into clientInfos
+                    clientInfos.Add(new ClientInfo(client.Id, client.Host, client.Port, numCompletedJobs));
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"Exception connect to {URL} failed : {e.Message}");
+                }
             }
-
-            return View(clientInfos);
+            return clientInfos;
         }
+
+        [HttpGet]
+        public IActionResult ClientInfo()
+        {
+            //return Ok(loadClientInfo());
+            List<ClientInfo> clientInfos = new List<ClientInfo>();
+            clientInfos.Add(new ClientInfo(1, "localhost", 8080, 10));
+            clientInfos.Add(new ClientInfo(2, "localhost", 8082, 2));
+            clientInfos.Add(new ClientInfo(3, "localhost", 8083, 5));
+            return Ok(clientInfos);
+        }
+
+
     }
 }
